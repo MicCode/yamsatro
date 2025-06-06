e0f1c0fb79ab3d10861653087e3a15badaddc67f
Mic <mic.guibert@free.fr>
2025-06-07 00:23:41 +0200

    Use loop to collect sum figures

--- globals/Game.gd
+++ globals/Game.gd
@@ -42,18 +42,9 @@ func update_active_figures():
 		values.append(val)
 		counts[val] = counts.get(val, 0) + 1
 
-	if counts.get(1, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_1)
-	if counts.get(2, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_2)
-	if counts.get(3, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_3)
-	if counts.get(4, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_4)
-	if counts.get(5, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_5)
-	if counts.get(6, 0) > 0:
-			active_figures.append(Enums.Figures.SUM_6)
+        for value in range(1, 7):
+                if counts.get(value, 0) > 0:
+                        active_figures.append(Enums.Figures["SUM_%d" % value])
 
 	var max_same := 0
 	for c in counts.values():
